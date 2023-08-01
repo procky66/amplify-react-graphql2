9849683530aa737db87ceec3cc559cac3a0965a2
procky66 <steveprocter69@googlemail.com>
2023-08-01 15:02:55 +0100

    Reduce image size

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ const App = ({ signOut }) => {
               <Image
                 src={note.image}
                 alt={`visual aid for ${notes.name}`}
-                style={{ width: 400 }}
+                style={{ width: 25 }}
               />
             )}
             <Button variation="link" onClick={() => deleteNote(note)}>
